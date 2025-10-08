9a64e158b6447b2fa53787bd306c9090e9a8a229
Dvodnenko <thekovalskii@gmail.com>
2025-10-08 12:26:22 +0300

    feat: turn popularity analyzer into service

--- src/services/analyzer.py
+++ src/services/analyzer.py
@@ -4,19 +4,22 @@ from ..models.insight import Insight
 from ..core.llm_client import LLMClient
 
 
-llm = LLMClient(model="mistral")
+class AnalyzerService:
 
-def analyze_popularity(articles: List[Article]) -> List[Insight]:
-    insights: List[Insight] = []
+    def __init__(self, model: LLMClient):
+        self._model = model
 
-    for article in articles:
-        prompt = (
-            f"Article: \"{article.title}\".\n"
-            f"Describe in short, why can this Article be popular"
-            f"on the platform {article.source}."
-        )
+    def analyze_popularity(self, articles: List[Article]) -> List[Insight]:
+        insights: List[Insight] = []
 
-        reason = llm.ask(prompt)
-        insights.append(Insight(article_title=article.title, reason=reason))
+        for article in articles:
+            prompt = (
+                f"Article: \"{article.title}\".\n"
+                f"Describe in short, why can this Article be popular"
+                f"on the platform {article.source}."
+            )
 
-    return insights
+            reason = self._model.ask(prompt)
+            insights.append(Insight(article_title=article.title, reason=reason))
+
+        return insights
