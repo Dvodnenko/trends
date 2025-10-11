349ceca044ae0c4f92d7c180985a1a4cace8c25d
Dvodnenko <thekovalskii@gmail.com>
2025-10-11 15:34:33 +0300

    feat: add database logic

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from .api import articles_router, insights_router
+from .db import mapping_registry, engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mapping_registry.metadata.create_all(engine)
+    yield
 
 
 application = FastAPI(
     title="Trends",
+    lifespan=lifespan
 )
 
 application.include_router(articles_router)
