9a5cf04c2a463e124fc9907b5309b18add8e1f21
Dvodnenko <thekovalskii@gmail.com>
2025-10-15 12:17:03 +0300

    refactor: switch to gemini LLM

--- src/api/dependencies/insights.py
+++ src/api/dependencies/insights.py
@@ -4,14 +4,13 @@ from fastapi import Depends
 
 from ...scrappers import get_reddit_article
 from ...services.analyzer import AnalyzerService
-from ...core.llm_client import LLMClient
 from ...schemas.insight import Insight
 
 
 def analyze_dep(
     id: str
 ):
-    service = AnalyzerService(LLMClient("mistral"))
+    service = AnalyzerService()
     r_article = get_reddit_article(post_id=id)
     insights = service.analyze_popularity([r_article], save=True)
     return insights
